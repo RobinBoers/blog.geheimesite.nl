a039c06e8d2276126fff8231ee75b4e5a3f7b34b
Robin Boers <robindev2019@outlook.com>
2022-03-06 20:10:33 +0100

    Add build script

---
+++ build.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+cd webdevelopment-en-meer/ && hugo --minify
+cd ../stupid-stuff-was-taken/ && hugo --minify
+cd .. && mv -f webdevelopment-en-meer/public ./public && mv -f stupid-stuff-was-taken/public ./public/en
