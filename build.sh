404ba0ad962b2a1801851d2c80b94996038a8faa
Robin Boers <robindev2019@outlook.com>
2022-03-08 07:36:49 +0100

    Move stupid-stuff-was-taken to notes

--- build.sh
+++ build.sh
@@ -2,6 +2,6 @@
 
 rm -rf public
 cd webdevelopment-en-meer/ && hugo --minify
-cd ../stupid-stuff-was-taken/ && hugo --minify
+cd ../notes/ && hugo --minify
 cd ../crzywebdev/ && hugo --minify
-cd .. && mv -f webdevelopment-en-meer/public ./public && mv -f stupid-stuff-was-taken/public ./public/notes && mv -f crzywebdev/public ./public/en
+cd .. && mv -f webdevelopment-en-meer/public ./public && mv -f notes/public ./public/notes && mv -f crzywebdev/public ./public/en
