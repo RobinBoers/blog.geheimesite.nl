88b5ae4151a18f1cfb560444bd9c0b761259d541
Robin Boers <robindev2019@outlook.com>
2024-04-07 21:41:53 +0200

    Migrate to Bakefile

--- .ci/server.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-# This scripts opens a local webserver at localhost:4000
-# and starts tailwind in the background
-
-npx tailwindcss -c tailwind.config.js -i src/assets/css/source.css -o src/assets/css/main.css -w &
-hugo server -DF -p 4000
